077b3343966c868065ce0e5557ad628c3895cd66
Andrei Noel Leyba <128600381+Andrei-NPI@users.noreply.github.com>
2024-02-08 11:03:42 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+// Create web browser
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('https://www.example.com');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
