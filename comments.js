99bbce3be9ea9727084a14d61343541352ebca00
Andrei Noel Leyba <128600381+Andrei-NPI@users.noreply.github.com>
2024-02-08 11:14:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,71 @@
-// create web server using express
-const express = require('express');
-const app = express();
-// create router object
-const router = express.Router();
+// create web server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+// create server
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var port = process.env.PORT || 3000;
+// create a body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// define route for get request
-router.get('/', (req, res) => {
-    res.send('Comments are available');
+// create a route
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-// define route for post request
-router.post('/', (req, res) => {
-    res.send('Comments are created');
+// create a route
+app.get('/comments', function(req, res) {
+    res.sendFile(__dirname + '/comments.json');
 });
 
-// define route for put request
-router.put('/', (req, res) => {
-    res.send('Comments are updated');
+// create a route
+app.post('/comments', function(req, res) {
+    fs.readFile(__dirname + '/comments.json', 'utf8', function(err, data) {
+        var comments = JSON.parse(data);
+        comments.push(req.body);
+        fs.writeFile(__dirname + '/comments.json', JSON.stringify(comments, null, 4), function(err) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(comments));
+        });
+    });
 });
 
-// define route for delete request
-router.delete('/', (req, res) => {
-    res.send('Comments are deleted');
+// create a route
+app.get('/bundle.js', function(req, res) {
+    res.sendFile(__dirname + '/bundle.js');
 });
 
-// export router object
-module.exports = router;
+// create a route
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + '/style.css');
+});
+
+// create a route
+app.get('/socket.io.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/socket.io-client/socket.io.js');
+});
+
+// create a route
+app.get('/socket.io-stream.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/socket.io-stream/socket.io-stream.js');
+});
+
+// create a route
+app.get('/socket.io-stream-client.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/socket.io-stream-client/socket.io-stream-client.js');
+});
+
+// create a route
+app.get('/socket.io-stream-client.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/socket.io-stream-client/socket.io-stream-client.js');
+});
+
+// create a route
+app.get('/socket.io-stream-client.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/socket.io-stream-client/socket.io-stream-client.js');
+});
+
